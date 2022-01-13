1e1fc91f8212ce610e04dc161f2c716b2f7b19c8
carlmachaalany <carlmachaalany1@gmail.com>
2022-01-13 00:33:52 -0500

    save likes on reload + worked out filter

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import 'bootstrap'
 import NavBar from './components/NavBar';
 import HeroArea from './components/HeroArea';
 import { Image } from './dtos/image';
-import Grid from '@mui/material/Grid';
-import { Card, CardActionArea, CardContent, CardMedia, ImageList, ImageListItem, Typography } from '@mui/material';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, CircularProgress, IconButton, ImageList, ImageListItem, LinearProgress, Typography } from '@mui/material';
+import { Animated } from 'react-animated-css';
+import {Favorite as FavoriteIcon, Share as ShareIcon} from '@mui/icons-material';
 
 function App() {
 
   const [filter, setFilter] = useState("all");
   const [allImages, setAllImages] = useState<Image[]>([]);
+  const [likedIndeces, setLikedIndeces] = useState<number[]>(localStorage.getItem("likedIndeces") ? JSON.parse(localStorage.getItem("likedIndeces") || "") : []);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  // const [likedImages, setLikedImages] = useState<Image[]>([]);
+  const [displayedImages, setDisplayedImages] = useState<Image[]>([]);
   
   const getGridColumns = () => {
     if (window.innerWidth <= 700) { return 1; }
@@ -20,16 +25,22 @@ function App() {
   const [gridColumns, setGridColumns] = useState(getGridColumns());
   
   const handleChangeFilter = () => {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
     console.log('Filter changed to:', filter);
+    setDisplayedImages(filter === 'all' ? allImages : allImages.filter((image, index) => likedIndeces.includes(index)))
+    console.log('New displayed images:', displayedImages);
   }
 
   useEffect(() => {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
     fetch("https://images-api.nasa.gov/search?q=''", {
         method: "GET"
     }).then(resp => {
       resp.json().then(lis => {
-        console.log(lis.collection.items);
-        setAllImages(lis.collection.items);
+        console.log(lis.collection.items.map((image: any) => { return {...image, liked: false}}));
+        setAllImages(lis.collection.items.map((image: any) => { return {...image, liked: false}}));
+        setDisplayedImages(lis.collection.items.map((image: any) => { return {...image, liked: false}}));
+        setIsLoading(false);
       });
     });
 
@@ -42,41 +53,71 @@ function App() {
   useEffect(() => {
     handleChangeFilter();
   }, [filter]);
+
+  const handleLike = (e: any, index: number) => {
+    console.log("liked a pic");
+    console.log("old likes ", likedIndeces);
+    e.target.classList.toggle('liked');
+    let newLikedIndeces;
+    if (likedIndeces.includes(index)) {
+      newLikedIndeces = likedIndeces.filter(i => i !== index);
+    } else {
+      newLikedIndeces =[...likedIndeces, index];
+    }
+    setLikedIndeces(newLikedIndeces);
+    console.log("new likes ", likedIndeces);
+    localStorage.setItem("likedIndeces", JSON.stringify(newLikedIndeces));
+  }
   
   return (
     <div className="App d-flex flex-column">
       <NavBar filter={filter} setFilter={setFilter} />
       <HeroArea />
-      <div id="body" className="align-self-center">
-        <ImageList className="mt-3" variant="masonry" cols={gridColumns} gap={8}>
-          {allImages.map((image: any) => (
-            <ImageListItem key={image.href}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    style={{
-                      backgroundPosition: "center center",
-                      backgroundRepeat: "no-repeat",
-                      backgroundSize: "cover",
-                    }}
-                    image={image.links[0].href}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {image.data[0].title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {image.data[0].description}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </div>
+      <h1 style={{fontSize: '2rem'}} className="mt-3"><strong>FEED</strong></h1>
+      <hr className="m-0 align-self-center" style={{width: "80%", borderTop: "3px solid #bbb"}}></hr>
+      {isLoading ?
+        <CircularProgress className="align-self-center" />
+      :
+        <div className="align-self-center justify-self-center" style={{width: "80%"}}>
+          <ImageList className="mt-3" variant="masonry" cols={gridColumns} gap={8}>
+            {displayedImages.map((image: any, index: number) => {
+              return (
+                <Animated key={image.href} animationIn="zoomIn" animationInDuration={1000} animationOut="fadeOut" isVisible={true}>
+                  <ImageListItem>
+                    <Card>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          style={{
+                            backgroundPosition: "center center",
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "cover",
+                          }}
+                          image={image.links[0].href}
+                          alt="green iguana"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="div">
+                            {image.data[0].title}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {image.data[0].description}
+                          </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                          <IconButton className={"like-button " + ((filter==='likes' || likedIndeces.includes(index)) ? "liked" : "")} onClick={(e) => handleLike(e, index)} aria-label="add to favorites">
+                            {/* <button className={"like-button" + (likedIndeces.includes(index)? "liked" : "")}></button> */}
+                          </IconButton>
+                        </CardActions>
+                      </CardActionArea>
+                    </Card>
+                  </ImageListItem>
+                </Animated>
+              )
+            })}
+          </ImageList>
+        </div>
+      }
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import {Animated} from 'react-animated-css';
 import starLogo from '../assets/star-logo2.png';
-import { FormControl, Select, MenuItem } from '@mui/material';
+import { FormControl, Select, MenuItem, FormControlLabel, Switch, FormGroup } from '@mui/material';
 
 interface Props {
     filter: string;
@@ -57,10 +57,10 @@ const NavBar: React.FC<Props> = ({filter, setFilter}) => {
                     component="div"
                     sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
                 >
-                    SPACESTAGRAM
+                    <strong>SPACESTAGRAM</strong>
                 </Typography>
             </Animated>
-            <FormControl style={{color: "#fff"}} sx={{ m: 1, minWidth: 120 }}>
+            <FormControl sx={{borderRadius: "10px", backgroundColor: "rgba(38, 34, 43, 0.8)", border: '1px solid #ced4da', minWidth: 100, maxHeight: 50}}>
                 <Select
                 style={{color: "#fff"}}
                 value={filter}
@@ -74,70 +74,6 @@ const NavBar: React.FC<Props> = ({filter, setFilter}) => {
             </FormControl>
         </Toolbar>
       </AppBar>
-        // <AppBar className="mb-4" position="static" style={{backgroundColor: background}}>
-        //     <Toolbar>
-        //         <Typography variant="h6" style={{flexGrow: 1}}>
-        //         News
-        //         </Typography>
-        //     </Toolbar>
-        // </AppBar>
-        // <header id="home" className="header">
-        //     <div className="navbar-area" style={{backgroundColor: `${background}`}}>
-        //         <div className="container">
-        //             {/* <div className="row"> */}
-        //                 {/* <div className="col-lg-12"> */}
-        //             <nav className="navbar navbar-expand-lg py-3">
-        //                 <a className="navbar-brand wow fadeInLeftBig slower" href="/">
-        //                     <img src={logo} alt="Logo" />
-        //                 </a>
-        //                 <Button onFocus={() => console.log('navbar focused')} variant="link" className="navbar-toggler collapsed" onClick={() => {
-        //                     // setOpen(!open);
-        //                     toggleHamburger();
-        //                 }} aria-controls="collapse-links" data-toggle="collapse" data-target="#collapse-links">
-        //                     <span className="toggler-icon"></span>
-        //                     <span className="toggler-icon"></span>
-        //                     <span className="toggler-icon"></span>
-        //                 </Button>
-                                                            
-        //                 <div className="navbar-collapse collapse" id="collapse-links">
-        //                     <ul id="nav"  className="nav navbar-nav ml-auto">
-        //                         <li className="nav-item active">
-        //                             {isLanding ? <a style={decorationNone} onClick={removeDropdown} className="page-scroll" href="#hero-area">Home</a> : <Link style={{ textDecoration: 'none' }} onClick={removeDropdown} to="/">Home</Link>}
-        //                             {/* <a onClick={removeDropdown} className="page-scroll" href="#hero-area">Home</a> */}
-        //                             {/* <Link onClick={removeDropdown} to="/">Home</Link> */}
-        //                         </li>
-        //                         <li className="nav-item">
-        //                             {/* <Link className="page-scroll" to="/#services">Services</Link> */}
-        //                             {/* <a onClick={removeDropdown} className="page-scroll" href="#services">Services</a> */}
-        //                             {isLanding ? 
-        //                             <ScrollLink onClick={removeDropdown} activeClass="active" to="services" offset={-100} spy={false} smooth={true} duration={500} style={{ cursor: "pointer", color: "black" }}>
-        //                                 Services 
-        //                             </ScrollLink> :
-        //                             <Link style={decorationNone} onClick={removeDropdown} to={{ pathname:"/", state: { section: "services"}}}>Services</Link> }
-        //                         </li>
-        //                         <li className="nav-item">
-        //                             <Link style={decorationNone} className="nav-links" onClick={removeDropdown} to="/fleet">Fleet</Link>
-        //                         </li>
-        //                         <li className="nav-item">
-        //                             {/* {isLanding ? 
-        //                             <a onClick={removeDropdown} className="page-scroll" href="#contact">Contact</a> : 
-        //                             <Link onClick={removeDropdown} to="/#contact">Contact</Link>} */}
-        //                             { isLanding ? 
-        //                             <ScrollLink onClick={removeDropdown} activeClass="active" to="contact" offset={-100} spy={false} smooth={true} duration={500} style={{ cursor: "pointer", color: "black" }}>
-        //                                 Contact 
-        //                             </ScrollLink> :
-        //                             <Link style={decorationNone} onClick={removeDropdown} to={{ pathname:"/", state: { section: "contact"}}}>Contact</Link>}
-
-        //                         </li>
-        //                         <li className="nav-item">
-        //                             <Link style={decorationNone} onClick={removeDropdown} to="/booking">Booking</Link>
-        //                         </li>
-        //                     </ul> 
-        //                 </div>
-        //             </nav>
-        //         </div>
-        //     </div>
-        // </header >
     );
 }
 
